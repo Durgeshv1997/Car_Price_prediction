43dea5e62f5c8b3340b85ce1577a251c6e9f61e9
Durgeshv1997 <Durgeshoatil1111@gmail.com>
2022-08-07 09:26:46 -0700

    edit_about_me

--- about.py
+++ about.py
@@ -9,6 +9,6 @@ def app():
     st.markdown('''### Name:
     Suchit Patil ''')
     st.markdown('''### Email:
-    suchitpatil2612@gmail.com''')
-    st.markdown('''### GitHub: [SuchitPatil](https://github.com/suchitpatil2612)''')
-    st.markdown('''### Lindin: [SuchitPatil](https://www.linkedin.com/in/suchit-p/)''')
+    durgeshpatil1111@gmail.com''')
+    st.markdown('''### GitHub: [Durgesh Patil](https://github.com/Durgeshv1997)''')
+    st.markdown('''### Lindin: [Durgesh Patil]()''')
