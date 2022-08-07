f590c48cc1787c25ac7486c179e3d705987442c2
Durgeshv1997 <Durgeshoatil1111@gmail.com>
2022-08-07 09:33:05 -0700

    about_name_edit

--- about.py
+++ about.py
@@ -7,7 +7,9 @@ def app():
     st.balloons()
     st.title('Contact Us')
     st.markdown('''### Name:
-    Suchit Patil ''')
+    Durgesh Patil ''')
+    st.markdown('''### Application No :
+    160266 ''')
     st.markdown('''### Email:
     durgeshpatil1111@gmail.com''')
     st.markdown('''### GitHub: [Durgesh Patil](https://github.com/Durgeshv1997)''')
